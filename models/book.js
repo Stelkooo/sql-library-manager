7215d08be8aee7dbf869344bc7615e8e85c8eab4
stelko <stilyanmtoskov@gmail.com>
2022-11-27 12:14:29 +0000

    book model created

---
+++ models/book.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {}
+  Book.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: '"Title" is required',
+        },
+      },
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: '"Author" is required',
+        },
+      },
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+
+  return Book;
+};
