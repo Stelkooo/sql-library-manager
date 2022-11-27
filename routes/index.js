d0bea980c0c17d4412e05b4d06116ba06f62568e
stelko <stilyanmtoskov@gmail.com>
2022-11-27 17:25:38 +0000

    root redirects to /books

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,9 @@ var express = require('express');
 var router = express.Router();
 const Book = require("../models").Book;
 
-/* Handler function to wrap each route. */
-function asyncHandler(cb) {
-  return async (req, res, next) => {
-    try {
-      await cb(req, res, next)
-    } catch (error) {
-      // Forward error to the global error handler
-      next(error);
-    }
-  }
-}
-
 /* GET home page. */
-router.get('/', asyncHandler( async (req, res, next) => {
-  
-}));
+router.get('/', (req, res) => {
+  res.redirect("/books");
+});
 
 module.exports = router;
