1dee34ab5a5fb10e221ddc92718b838972de3c0c
stelko <stilyanmtoskov@gmail.com>
2022-11-27 17:23:14 +0000

    users.js to books

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/books');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 });
 
 // global error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   err.status = err.status ? err.status : 500;
   err.message = "Sorry! There was an unexpected error on the server.";
 
